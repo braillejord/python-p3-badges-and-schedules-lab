0d1d6e7d39bf721e55929d2289dd6e1d489b46e8
BreElle Wells <breellejordyn@gmail.com>
2023-08-22 12:52:19 -0600

    practice iterating through lists

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,26 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
+
 
 def batch_badge_creator(names):
-    return None
+    name_list = [f"Hello, my name is {name}." for name in names]
+    return name_list
+
 
 def assign_rooms(names):
-    return None
+    room_list = []
+
+    for index, item in enumerate(names):
+        room_list.append(f"Hello, {item}! You'll be assigned to room {index + 1}!")
+
+    return room_list
+
 
 def printer(names):
-    return None
+    name_list = batch_badge_creator(names)
+    for name in name_list:
+        print(name)
+
+    room_list = assign_rooms(names)
+    for assignment in room_list:
+        print(assignment)
